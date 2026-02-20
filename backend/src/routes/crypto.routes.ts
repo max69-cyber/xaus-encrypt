6ff44e889f30ba273211f65ff1414893f436a6ec
max69-cyber <rayxaus@gmail.com>
2026-02-20 22:33:18 +0500

    backend: fixing file transfer

--- backend/src/routes/crypto.routes.ts
+++ backend/src/routes/crypto.routes.ts
@@ -6,15 +6,21 @@ import {
     encryptFileHandler, decryptFileHandler
 } from "../controllers/crypto.controller"
 import {authMiddleware} from "../middleware/auth.middleware";
+import multer from "multer";
 
 const cryptoRouter = Router()
+
+// using ram
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
 
 cryptoRouter.get('/history', authMiddleware, getHistoryHandler)
 
 cryptoRouter.post("/encrypt-text", authMiddleware, encryptTextHandler)
 cryptoRouter.post("/decrypt-text", authMiddleware, decryptTextHandler)
 
-cryptoRouter.post("/encrypt-file", authMiddleware, encryptFileHandler)
-cryptoRouter.post("/decrypt-file", authMiddleware, decryptFileHandler)
+cryptoRouter.post("/encrypt-file", authMiddleware, upload.single("file"), encryptFileHandler)
+cryptoRouter.post("/decrypt-file", authMiddleware, upload.single("file"), decryptFileHandler)
 
 export default cryptoRouter
